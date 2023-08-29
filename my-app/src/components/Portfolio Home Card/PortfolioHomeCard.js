cf24b0c1b6f74572771f9e745ffc08da704d965b
Kimberly <kimhnguyeno@g.ucla.edu>
2023-08-29 14:38:07 -0700

    Testing routing 4.

--- my-app/src/components/Portfolio Home Card/PortfolioHomeCard.js
+++ my-app/src/components/Portfolio Home Card/PortfolioHomeCard.js
@@ -5,12 +5,12 @@ function PortfolioHomeCard(props) {
   const title = props.title;
   const category = props.categ;
   const descr = props.descr;
-  const link = '/' + props.link;
+  const link = props.link;
   const imgSrc = props.imgSrc;
 
   return(
     <div className='proj' id={props.id}>
-      <Link to={link}>
+      <Link to={props.link}>
         <img src={props.imgSrc}></img>
         <div className='card-overlay'>
           <div className='header'>
